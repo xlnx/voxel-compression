6e064599b3c942d358e5836e10450428e2acbb6b
Shuoliu Yang <yangshuoliu@sina.com>
2019-11-13 19:49:55 +0800

    windows

--- src/video_compress/devices/d3d9_encoder.hpp
+++ src/video_compress/devices/d3d9_encoder.hpp
@@ -6,19 +6,17 @@ namespace vol
 {
 VM_BEGIN_MODULE( video )
 
-struct D3D9EncoderImpl;
-
 struct D3D9Encoder : Encoder
 {
-	D3D9Encoder( uint32_t width, uint32_t height, PixelFormat format );
-	~D3D9Encoder();
-	void encode( Reader &reader, Writer &writer ) override;
-
-protected:
-	void *get_nv_impl() override;
-
-private:
-	vm::Box<D3D9EncoderImpl> _;
+	D3D9Encoder( uint32_t width, uint32_t height, PixelFormat format )
+	{
+	}
+	~D3D9Encoder()
+	{
+	}
+	void encode( Reader &reader, std::vector<char> &frames, std::vector<uint32_t> &frame_len ) override
+	{
+	}
 };
 
 VM_END_MODULE()
