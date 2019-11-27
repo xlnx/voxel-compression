5f48a62df76c971b2651020a31a361bcc9331854
koishi <qwq@koishi.top>
2019-11-27 15:15:21 +0800

    cxxopts voxel-info

--- tools/voxel-info.cc
+++ tools/voxel-info.cc
@@ -1,4 +1,5 @@
 #include <fstream>
+#include "cxxopts.hpp"
 #include <VMUtils/fmt.hpp>
 #include <vocomp/refine/extractor.hpp>
 #include <vocomp/io.hpp>
@@ -7,13 +8,25 @@ using namespace std;
 
 int main( int argc, char **argv )
 {
+	cxxopts::Options options( "voxel-info", "Print voxel info" );
+	options.add_options()(
+	  "i,input", "input compressed file", cxxopts::value<string>() )(
+	  "h,help", "print this help message" )(
+	  "l,list", "list indices" );
+
+	auto opts = options.parse( argc, argv );
+	if ( opts.count( "h" ) || !opts.count( "i" ) ) {
+		vm::println( "{}", options.help() );
+		return 0;
+	}
+
 	try {
-		ifstream in( argv[ 1 ], std::ios::ate | std::ios::binary );
+		ifstream in( opts[ "i" ].as<string>(), std::ios::ate | std::ios::binary );
 		auto len = in.tellg();
 		vol::StreamReader reader( in, 0, len );
 		vol::refine::Extractor e( reader );
 
-		if ( argc <= 2 ) {
+		if ( !opts.count( "l" ) ) {
 			vm::println( "{>16}: {}", "Size", e.raw() );
 			vm::println( "{>16}: {}", "Padded Size", e.adjusted() );
 			vm::println( "{>16}: {}", "Grid Size", e.dim() );
